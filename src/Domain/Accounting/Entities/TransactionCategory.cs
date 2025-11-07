532fcad4b5ce1587291ecf97261b32de51087f51
Vitaliy <VitaliyThupin@gmail.com>
2025-11-07 19:22:34 +0200

    refactor(domain): simplify TransactionCategory factory methods
    - Replaced `LTree` with `CategoryPath` for enhanced domain modeling.
    - Streamlined constructors and removed unused parameters for clarity.
    - Updated factory methods for root and child category creation.

--- src/Domain/Accounting/Entities/TransactionCategory.cs
+++ src/Domain/Accounting/Entities/TransactionCategory.cs
@@ -1,35 +1,23 @@
 ﻿namespace Domain.Accounting.Entities;
 
-public sealed class TransactionCategory : CategoryNode<TransactionCategoryId, TransactionCategory>, IAggregateRoot
+public sealed class TransactionCategory : BaseCategory<TransactionCategoryId, TransactionCategory>, IAggregateRoot
 {
+    #region Constructors
     private TransactionCategory() { }
-    private TransactionCategory(TransactionCategoryId id, string name, LTree path, DateTime createdDate, TransactionCategoryId? parentId)
+    private TransactionCategory(TransactionCategoryId id, string name, CategoryPath path)
     {
-        Id = Guard.Against.Default(id, nameof(id));
+        SetCategoryId(id);
         SetName(name);
-        SetParent(parentId);
         SetPath(path);
-        MarkAsCreated(createdDate);
     }
+    #endregion
 
-    public static TransactionCategory Create(TransactionCategoryId id, string name, LTree path, DateTime createdAt,
-        TransactionCategoryId? parentId = null)
-        => new(id, name, path, createdAt, parentId);
-
-    public static TransactionCategory CreateRoot(TransactionCategoryId id, string name, DateTime createdAt)
-        => new(
-            id: id,
-            name: name,
-            path: (LTree)$"root.n{id}",
-            createdDate: createdAt,
-            parentId: null);
-
-    public static TransactionCategory CreateChild(TransactionCategoryId id, string name, DateTime createdAt,
-        TransactionCategory parent)
-        => new(
-            id: id,
-            name: name,
-            path: parent.Path + (LTree)$".n{id}",
-            createdDate: createdAt,
-            parentId: parent.Id);
+    #region Factories
+    public static TransactionCategory Create(TransactionCategoryId id, string name, CategoryPath path)
+        => new(id, name, path);
+    public static TransactionCategory CreateRoot(TransactionCategoryId id, string name)
+        => new(id, name, CategoryPath.Root(id.ToString()));
+    public static TransactionCategory CreateChild(TransactionCategoryId id, string name, TransactionCategory parent)
+        => new(id, name, parent.Path.Append(id.ToString()));
+    #endregion
 }
