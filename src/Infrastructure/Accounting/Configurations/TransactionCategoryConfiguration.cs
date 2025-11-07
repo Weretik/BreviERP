ea85f73e04f4c1f00c844d776c6bf86202f9bef0
Vitaliy <VitaliyThupin@gmail.com>
2025-11-07 19:23:03 +0200

    feat(infrastructure): enhance TransactionCategory configuration with value conversion
    - Added `IdConverter` for `TransactionCategoryId` with `ValueGeneratedNever`.
    - Updated configuration to ensure seamless EF Core integration.
    - Replaced `StronglyTypedId` with `ValueObject<int>` for `TransactionId` for consistency.

--- src/Infrastructure/Accounting/Configurations/TransactionCategoryConfiguration.cs
+++ src/Infrastructure/Accounting/Configurations/TransactionCategoryConfiguration.cs
@@ -1,8 +1,16 @@
-﻿namespace Infrastructure.Accounting.Configurations;
+﻿using Infrastructure.Accounting.Convertors;
+
+namespace Infrastructure.Accounting.Configurations;
 
 public sealed class TransactionCategoryConfiguration : IEntityTypeConfiguration<TransactionCategory>
 {
     public void Configure(EntityTypeBuilder<TransactionCategory> builder)
-        => CategoryMapping.Configure<TransactionCategory, TransactionCategoryId>(
+    {
+        CategoryMapping.Configure<TransactionCategoryId, TransactionCategory>(
             builder, tableName: "TransactionCategories");
+
+        builder.Property(c => c.Id)
+            .HasConversion(IdConverter.TransactionCategoryIdConvert)
+            .ValueGeneratedNever();
+    }
 }
