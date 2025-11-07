172e8c4956138242851cc50a0fa8ba3df3db989a
Vitaliy <VitaliyThupin@gmail.com>
2025-11-07 19:22:41 +0200

    refactor(domain): simplify TransactionType and streamline instantiation
    - Replaced derived classes with a single sealed class for clarity.
    - Consolidated properties into constructor for more concise initialization.
    - Updated static instances to use the new streamlined constructor.

--- src/Domain/Accounting/Enumerations/TransactionType.cs
+++ src/Domain/Accounting/Enumerations/TransactionType.cs
@@ -1,25 +1,10 @@
 ﻿namespace Domain.Accounting.Enumerations;
 
-public abstract class TransactionType(string name, int value) : SmartEnum<TransactionType>(name, value)
+public sealed class TransactionType(string name, int value, string description, char symbol ) : SmartEnum<TransactionType>(name, value)
 {
-    public abstract string TypeName { get; }
-    public abstract string Description { get; }
-    public abstract char Symbol { get; }
+    public string Description { get; } = description;
+    public char Symbol { get; } = symbol;
 
-    public static readonly TransactionType Income  = new IncomeType();
-    public static readonly TransactionType Expense = new ExpenseType();
-
-    private sealed class IncomeType() : TransactionType(nameof(Income), 1)
-    {
-        public override string TypeName => "Дохід";
-        public override string Description => "Операція, для збільшення балансу";
-        public override char Symbol => '+';
-    }
-
-    private sealed class ExpenseType() : TransactionType(nameof(Expense), 2)
-    {
-        public override string TypeName => "Витрата";
-        public override string Description => "Операція, для зменшення балансу";
-        public override char Symbol => '-';
-    }
+    public static readonly TransactionType Income  = new("Дохід", 1, "Операція, для збільшення балансу", '+' );
+    public static readonly TransactionType Expense = new("Витрата", 2, "Операція, для зменшення балансу", '-');
 }
