8a6fd826555006919c9aa11e812a690acb3d7456
Vitaliy <VitaliyThupin@gmail.com>
2025-11-07 19:22:19 +0200

    feat(infrastructure): add value converters for ids and enums
    - Introduced `IdConverter` for handling `TransactionId` and `TransactionCategoryId`.
    - Added `EnumConverter` for `TransactionType` to enable seamless EF Core integration.

---
+++ src/Infrastructure/Accounting/Convertors/EnumConverter.cs
@@ -0,0 +1,14 @@
+using Domain.Accounting.Enumerations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Accounting.Convertors;
+
+public class EnumConverter
+{
+    public static readonly ValueConverter<TransactionType, string> PriceTypeConvert =
+        new(
+            priceType => priceType.Name,
+            name => TransactionType.FromName(name, false)
+        );
+
+}
---
+++ src/Infrastructure/Accounting/Convertors/IdConverter.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Accounting.Convertors;
+
+public class IdConverter
+{
+    public static readonly ValueConverter<TransactionId, int> TransactionIdConvert =
+        new(
+            id => id.Value,
+            value => TransactionId.From(value)
+        );
+    public static readonly ValueConverter<TransactionCategoryId, int> TransactionCategoryIdConvert =
+        new(
+            id => id.Value,
+            value => TransactionCategoryId.From(value)
+        );
+}
