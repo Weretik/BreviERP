60b54ae41e173d0962d40c1e44f75c7faa31f11a
Vitaliy <VitaliyThupin@gmail.com>
2025-11-10 18:05:09 +0200

    feat(domain): add Percent value object with validation and utility methods
    - Introduced `Percent` value object to encapsulate percentage logic.
    - Includes validation to ensure values are between 0 and 100.
    - Added utility methods for percentage calculation, discount, and change application.

---
+++ src/Domain/Reference/ValueObjects/Percent.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Reference.ValueObjects;
+
+[ValueObject<decimal>]
+public readonly partial struct Percent
+{
+    private static Validation Validate(decimal value) =>
+     value is >= 0m and <= 100m
+         ? Validation.Ok
+         : Validation.Invalid("Percent must be between 0 and 100.");
+
+    // 15% of 2000 = 300
+    public decimal Of(decimal amount) => amount * Value / 100m;
+
+    // 2000 + 15% = 2300
+    public decimal ApplyChange(decimal amount) => amount * (1m + Value / 100m);
+
+    // 2000 - 15% = 1700
+    public decimal ApplyDiscount(decimal amount) => amount * (1m - Value / 100m);
+}
