bc420809df0f9b8f3f1ff0d681658f67c255a521
Vitaliy <VitaliyThupin@gmail.com>
2025-11-10 18:04:15 +0200

    feat(infrastructure): add configuration for AdditionalReference
    - Introduced `AdditionalReferenceConfiguration` for EF Core setup.
    - Ensures proper infrastructure layering and entity configuration alignment.

---
+++ src/Infrastructure/Reference/Congigurations/AdditionalReferenceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Reference.Entities;
+
+namespace Infrastructure.Reference.Congigurations;
+
+
+public sealed class AdditionalReferenceConfiguration : IEntityTypeConfiguration<AdditionalReference>
+{
+    public void Configure(EntityTypeBuilder<AdditionalReference> builder)
+    {
+
+
+    }
+}
