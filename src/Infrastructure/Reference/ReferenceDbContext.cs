38e9e157c32cc62ab80c1ba73f321da8a170c969
Vitaliy <VitaliyThupin@gmail.com>
2025-11-10 18:06:49 +0200

    feat(infrastructure): add ReferenceDbContext for managing references
    - Introduced `ReferenceDbContext` with EF Core configurations.
    - Supports `AdditionalReference` management and infrastructure extension.

---
+++ src/Infrastructure/Reference/ReferenceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Reference.Entities;
+
+namespace Infrastructure.Reference;
+
+public class ReferenceDbContext(DbContextOptions<ReferenceDbContext> options) : DbContext(options)
+{
+    public DbSet<AdditionalReference> AdditionalReferences => Set<AdditionalReference>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ReferenceDbContext).Assembly,
+            type => type.Namespace?.StartsWith("Infrastructure.Reference") ?? false);
+    }
+}
