c0347cd05d4d211caccecfde29950b5203ed8027
Vitaliy <VitaliyThupin@gmail.com>
2025-11-13 17:05:14 +0200

    feat(infrastructure): add DiscardChanges method to ReferenceDbContext
    - Introduced `DiscardChanges` for clearing EF Core's ChangeTracker.
    - Improves ability to discard uncommitted changes in the context, enhancing flexibility in change management.

--- src/Infrastructure/Reference/ReferenceDbContext.cs
+++ src/Infrastructure/Reference/ReferenceDbContext.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Reference;
 public class ReferenceDbContext(DbContextOptions<ReferenceDbContext> options) : DbContext(options)
 {
     public DbSet<AdditionalReference> AdditionalReferences => Set<AdditionalReference>();
-
+    public void DiscardChanges() => ChangeTracker.Clear();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
