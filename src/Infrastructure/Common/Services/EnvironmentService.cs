aeb192a2069f16132d9e98b6ba8d6645c6bb293a
Vitaliy <VitaliyThupin@gmail.com>
2025-11-13 18:00:15 +0200

    refactor(infrastructure): remove unused services and interface dependencies
    - Deleted unused infrastructure services and application interfaces like `EnvironmentService`, `ICurrentUserService`, `ICacheService`, and others.
    - Streamlines codebase by eliminating obsolete abstractions, improving maintainability.

--- src/Infrastructure/Common/Services/EnvironmentService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Infrastructure.Common.Services;
-
-public class EnvironmentService(IWebHostEnvironment env) : IEnvironmentService
-{
-    public bool IsProduction() => env.IsProduction();
-}
