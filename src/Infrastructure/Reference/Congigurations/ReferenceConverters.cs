ad8510666f3ed239285a70d3931ac35d7df9a86a
Vitaliy <VitaliyThupin@gmail.com>
2025-11-13 16:09:15 +0200

    refactor(infrastructure): update and extend reference converters
    - Renamed `IdConverter` to `ReferenceConverters` for clarity and reusability.
    - Added converters for `Percent` and `Money` value objects.
    - Enhances extensibility and maintains consistency in EF Core configurations.

---
+++ src/Infrastructure/Reference/Congigurations/ReferenceConverters.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Reference.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Reference.Congigurations;
+
+public static class ReferenceConverters
+{
+    public static readonly ValueConverter<Percent, decimal> PercentConvert =
+        new(
+            p => p.Value,
+            v => Percent.From(v)
+        );
+
+    public static readonly ValueConverter<AdditionalReferenceId, int> AdditionalReferenceIdConvert =
+        new(
+            id => id.Value,
+            v => AdditionalReferenceId.From(v)
+        );
+
+    public static readonly ValueConverter<Money, decimal> MoneyConvert =
+        new(
+            m => m.Amount,
+            v => new Money(v, "UAH")
+        );
+}
