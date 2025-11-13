cd4940c065dcae2e8a867ff2eddc032b980b8a34
Vitaliy <VitaliyThupin@gmail.com>
2025-11-13 17:05:24 +0200

    feat(infrastructure): add EF repository implementations for references
    - Added `ReferenceEfRepository` for managing reference data operations.
    - Introduced `ReferenceReadEfRepository` to handle read operations with specifications.
    - Enhances the repository pattern implementation in the infrastructure layer.

---
+++ src/Infrastructure/Reference/Repositories/ReferenceEfRepository.cs
@@ -0,0 +1,7 @@
+﻿using Application.Reference.Shared;
+
+namespace Infrastructure.Reference.Repositories;
+
+internal sealed class ReferenceEfRepository<T>(ReferenceDbContext dbContext)
+    : RepositoryBase<T>(dbContext), IReferenceRepository<T>
+    where T : class, IAggregateRoot { }
