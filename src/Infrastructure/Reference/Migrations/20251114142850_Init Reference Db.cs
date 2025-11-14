2eed457832f36f181646235f4eaa455f930195a3
Vitaliy <VitaliyThupin@gmail.com>
2025-11-14 16:29:40 +0200

    feat(infrastructure): add initial database migrations for reference and identity
    - Introduced initial migrations for ReferenceDb and IdentityDb to set up database structure.
    - Provides foundation for domain entities and identity management system.

---
+++ src/Infrastructure/Reference/Migrations/20251114142850_Init Reference Db.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Reference.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitReferenceDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdditionalReferences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    SeamstressCount = table.Column<int>(type: "integer", nullable: false),
+                    SeamstressAverageSalary = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    MonthlyExpenses = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    CuttingFactor = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    MasterFactor = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    SeamstressBonusFactor = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    WorkshopManagerFactor = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    PpeAbove40UnitsProfit = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    PpeTenTo40UnitsProfit = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    PpeUpTo10UnitsProfit = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    ProducedAbove40UnitsProfit = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    ProducedTenTo40UnitsProfit = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    ProducedUpTo10UnitsProfit = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdditionalReferences", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdditionalReferences");
+        }
+    }
+}
