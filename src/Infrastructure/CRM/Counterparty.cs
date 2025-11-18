39078285eb5132cfd45e6e75aa062ae448575b4c
Vitaliy <VitaliyThupin@gmail.com>
2025-11-18 12:35:49 +0200

    feat(infrastructure): add CounterpartyDbContext for CRM module
    - Introduced `CounterpartyDbContext` to manage Counterparty entities within the CRM module.
    - Implemented entity configuration discovery to streamline setup.
    - Added `DiscardChanges` method to improve change tracking handling.

---
+++ src/Infrastructure/CRM/Counterparty.cs
@@ -0,0 +1,18 @@
+﻿using Domain.CRM.Entities;
+
+namespace Infrastructure.CRM;
+
+public class CounterpartyDbContext(DbContextOptions<CounterpartyDbContext> options) : DbContext(options)
+{
+    public DbSet<Counterparty> Counterparty => Set<Counterparty>();
+    public void DiscardChanges() => ChangeTracker.Clear();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(CounterpartyDbContext).Assembly,
+            type => type.Namespace?.StartsWith("Infrastructure.CRM") ?? false);
+    }
+}
