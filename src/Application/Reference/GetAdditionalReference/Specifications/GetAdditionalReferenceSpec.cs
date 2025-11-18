027ca92acfd4883414c2665d7b99d0f914da812d
Vitaliy <VitaliyThupin@gmail.com>
2025-11-18 12:08:41 +0200

    refactor(application): simplify additional reference specification
    - Removed DTO projection from `GetAdditionalReferenceSpec` to streamline logic.
    - Used value object `AdditionalReferenceId` for cleaner and more consistent querying.

--- src/Application/Reference/GetAdditionalReference/Specifications/GetAdditionalReferenceSpec.cs
+++ src/Application/Reference/GetAdditionalReference/Specifications/GetAdditionalReferenceSpec.cs
@@ -1,30 +1,13 @@
 ﻿using Domain.Reference.Entities;
+using Domain.Reference.ValueObjects;
 
 namespace Application.Reference.GetAdditionalReference;
 
-public class GetAdditionalReferenceSpec : Specification<AdditionalReference, AdditionalReferenceDTO>
+public class GetAdditionalReferenceSpec : Specification<AdditionalReference>
 {
     public GetAdditionalReferenceSpec()
     {
         Query.AsNoTracking()
-            .Where(x => x.Id == 1)
-            .Select(x => new AdditionalReferenceDTO(
-                x.SeamstressCount,
-                x.SeamstressAverageSalary.Amount,
-                x.MonthlyExpenses.Amount,
-                new LaborOverheadFactorsDTO(
-                    x.Factors.Cutting.Value,
-                    x.Factors.Master.Value,
-                    x.Factors.WorkshopManager.Value,
-                    x.Factors.SeamstressBonus.Value),
-                new ProfitFactorsDTO(
-                    x.ProducedProfit.Above40Units.Value,
-                    x.ProducedProfit.TenTo40Units.Value,
-                    x.ProducedProfit.UpTo10Units.Value),
-                new ProfitFactorsDTO(
-                    x.PpeProfit.Above40Units.Value,
-                    x.PpeProfit.TenTo40Units.Value,
-                    x.PpeProfit.UpTo10Units.Value)
-                ));
+            .Where(x => x.Id == AdditionalReferenceId.From(1));
     }
 }
