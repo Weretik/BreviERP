c3cd6db1dcb4d88cbcc55b6de095024121873be8
Vitaliy <VitaliyThupin@gmail.com>
2025-11-18 13:01:03 +0200

    feat(infrastructure): add Counterparty entity configurations
    - Introduced `CounterpartyConfiguration` to define database schema for Counterparty entities.
    - Added `CrmConverter` utility for value conversions of CounterpartyId and CounterpartyType.
    - Enhances entity mapping and ensures proper conversion handling.

---
+++ src/Infrastructure/CRM/Configurations/CounterpartyConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.CRM.Entities;
+
+namespace Infrastructure.CRM.Configurations;
+
+public class CounterpartyConfiguration : IEntityTypeConfiguration<Counterparty>
+{
+    public void Configure(EntityTypeBuilder<Counterparty> builder)
+    {
+        builder.ToTable("Counterparties");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(CrmConverter.CounterpartyIdConvert)
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.Type)
+            .HasConversion(CrmConverter.CounterpartyTypeConvert)
+            .HasMaxLength(50)
+            .HasColumnName("Type")
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
---
+++ src/Infrastructure/CRM/Configurations/CounterpartyConverter.cs
@@ -0,0 +1,21 @@
+﻿using Domain.CRM.Enums;
+using Domain.CRM.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.CRM.Configurations;
+
+public static class CrmConverter
+{
+    public static readonly ValueConverter<CounterpartyId, int> CounterpartyIdConvert =
+    new(
+        id => id.Value,
+        value => CounterpartyId.From(value)
+    );
+
+    public static readonly ValueConverter<CounterpartyType, string> CounterpartyTypeConvert =
+        new(
+            type => type.Name,
+            value => CounterpartyType.FromName(value, false)
+        );
+
+}
