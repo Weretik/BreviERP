07f5d0f76531c8a5ab6a52fadf9515618726b51d
Vitaliy <VitaliyThupin@gmail.com>
2025-11-18 12:35:37 +0200

    feat(domain): add validation for CounterpartyId value object
    - Introduced validation logic to ensure `CounterpartyId` is positive and within a valid range.
    - Enhances data integrity by preventing invalid IDs from being used.

--- src/Domain/CRM/ValueObjects/Counterparty.cs
+++ src/Domain/CRM/ValueObjects/Counterparty.cs
@@ -1,4 +1,12 @@
 ﻿namespace Domain.CRM.ValueObjects;
 
 [ValueObject<int>]
-public partial struct CounterpartyId { };
+public partial struct CounterpartyId
+{
+    private static Validation Validate(int value)
+    {
+        if (value <= 0) return Validation.Invalid("Id must be positive.");
+        if (value > 1_000_000_000) return Validation.Invalid("Id is too large.");
+        return Validation.Ok;
+    }
+};
