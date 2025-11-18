83838253bfe79ffd64ae1502f85d9f76304c865f
Vitaliy <VitaliyThupin@gmail.com>
2025-11-18 13:00:53 +0200

    feat(infrastructure): add Counterparty repositories
    - Implemented `CounterpartyEfRepository` and `CounterpartyReadEfRepository` to manage Counterparty entity interactions in the CRM module.
    - Enhances data access layer by introducing read and write separation for Counterparty entities.

---
+++ src/Infrastructure/CRM/Repositories/CounterpartyEfRepository.cs
@@ -0,0 +1,7 @@
+﻿using Application.CRM.Shared.Contracts;
+
+namespace Infrastructure.CRM.Repositories;
+
+public sealed class CounterpartyEfRepository<T>(CounterpartyDbContext dbContext)
+    : RepositoryBase<T>(dbContext), ICounterpartyRepository<T>
+    where T : class, IAggregateRoot { }
