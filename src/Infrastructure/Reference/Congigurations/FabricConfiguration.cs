c63a4f86ca5ed79532a06bc48a4a99c253fec171
Vitaliy <VitaliyThupin@gmail.com>
2025-11-19 19:45:09 +0200

    feat(infrastructure): add FabricsReference table and configuration
    - Introduced `FabricsReference` table with columns for `Id`, `Name`, `CounterpartyId`, and `Price`.
    - Added migration and configuration for Fabrics to align with domain model.
    - Supports enhanced management of fabrics within the Reference DB.

---
+++ src/Infrastructure/Reference/Congigurations/FabricConfiguration.cs
@@ -0,0 +1,29 @@
+using Domain.Reference.Entities;
+
+namespace Infrastructure.Reference.Congigurations;
+
+public sealed class FabricConfiguration : IEntityTypeConfiguration<Fabric>
+{
+    public void Configure(EntityTypeBuilder<Fabric> builder)
+    {
+        builder.ToTable("FabricsReference");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(ReferenceConverters.FabricIdConvert)
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.CounterpartyId)
+            .IsRequired();
+
+        builder.Property(x => x.Price)
+            .HasConversion(ReferenceConverters.MoneyConvert)
+            .HasPrecision(18, 2)
+            .IsRequired();
+    }
+}
---
+++ src/Infrastructure/Reference/Migrations/20251119173820_UpdateReference_20251119.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Reference.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReference_20251119 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FabricsReference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    CounterpartyId = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FabricsReference", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FabricsReference");
+        }
+    }
+}
